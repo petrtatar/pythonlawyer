9144adaca3fd0620a041916e47866c92ab232448
Peter Tatar <tatar.petr523@gmail.com>
2024-09-14 14:28:18 +0300

    Echo-bot which replies to a messages and basic commands (start and help)

--- some_file.py
+++ some_file.py
@@ -1,3 +1,27 @@
-print('This is file im repository')
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
 
-print('This is a new lical change')
+BOT_TOKEN = '7321879151:AAHgnM9nWtnMF41uGp8GS5hEeiaMRVYbYuM'
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.reply(text='Эхо-бот')
+
+
+@dp.message(Command(commands=['help']))
+async def cmd_help(message: Message):
+    await message.reply('Напиши мне сообщение и я его повторю')
+
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
