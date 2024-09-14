7c216999e8b8be414f974c317c91e2883a48617a
Peter Tatar <tatar.petr523@gmail.com>
2024-09-14 16:39:00 +0300

    Added handler sending the photo back

--- some_file.py
+++ some_file.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command, CommandStart
-from aiogram.types import Message, ContentType
+from aiogram.types import Message
 
 BOT_TOKEN = '7321879151:AAHgnM9nWtnMF41uGp8GS5hEeiaMRVYbYuM'
 
@@ -8,20 +8,24 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
+@dp.message(CommandStart())
 async def cmd_start(message: Message):
     await message.reply(text='Эхо-бот')
 
 
+@dp.message(Command(commands=['help']))
 async def cmd_help(message: Message):
     await message.reply('Напиши мне сообщение и я его повторю')
 
 
+@dp.message(F.photo)
+async def send_echo_photo(message: Message):
+    await message.answer_photo(message.photo[-1].file_id)
+
+
+@dp.message()
 async def send_echo_message(message: Message):
     await message.reply(text=message.text)
-
-dp.message.register(cmd_start, CommandStart())
-dp.message.register(cmd_help, Command(commands=['help']))
-dp.message.register(send_echo_message)
 
 
 if __name__ == '__main__':
